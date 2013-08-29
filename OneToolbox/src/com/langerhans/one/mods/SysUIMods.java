19a0f8555ee1097d3f6fe9365e171c2319db0914
langerhans <max.keller@gmx.com>
2013-08-29 21:16:31 +0200

    new sb transparency approach

--- OneToolbox/src/com/langerhans/one/mods/SysUIMods.java
+++ OneToolbox/src/com/langerhans/one/mods/SysUIMods.java
@@ -2,26 +2,38 @@ package com.langerhans.one.mods;
 
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 import android.content.res.XModuleResources;
+import android.content.res.XResources;
+import android.graphics.drawable.Drawable;
 import android.view.View;
 import android.widget.ImageView;
 
 import com.langerhans.one.R;
 
 import de.robv.android.xposed.XC_MethodHook;
-import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.callbacks.XC_InitPackageResources.InitPackageResourcesParam;
 import de.robv.android.xposed.callbacks.XC_LayoutInflated;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
 public class SysUIMods{
 
-	public static void execHook_InvisiBar(final InitPackageResourcesParam resparam, String MODULE_PATH, final int transparency) {
-		resparam.res.hookLayout("com.android.systemui", "layout", "super_status_bar", new XC_LayoutInflated() {
+	public static void execHook_InvisiBar(final InitPackageResourcesParam resparam, final String MODULE_PATH, final int transparency) {
+//		resparam.res.hookLayout("com.android.systemui", "layout", "super_status_bar", new XC_LayoutInflated() {
+//			@Override
+//			public void handleLayoutInflated(LayoutInflatedParam liparam) throws Throwable {
+//				View bg = liparam.view.findViewById(resparam.res.getIdentifier("status_bar", "id", "com.android.systemui"));
+//				bg.getBackground().setAlpha(transparency);
+//			}
+//		});
+		
+		resparam.res.setReplacement("com.android.systemui", "drawable", "status_bar_background", new XResources.DrawableLoader() {
 			@Override
-			public void handleLayoutInflated(LayoutInflatedParam liparam) throws Throwable {
-				View bg = liparam.view.findViewById(resparam.res.getIdentifier("status_bar", "id", "com.android.systemui"));
-				bg.getBackground().setAlpha(transparency);
+			public Drawable newDrawable(XResources res, int id)	throws Throwable {
+				XModuleResources modRes = XModuleResources.createInstance(MODULE_PATH, resparam.res);
+				Drawable sb = modRes.getDrawable(R.drawable.status_bar_background);
+				sb.setAlpha(transparency);
+				return sb;
 			}
+			
 		});
 	}
 
@@ -52,7 +64,7 @@ public class SysUIMods{
 		});
 	}
 
-	public static void execHook_InvisiNotify(final InitPackageResourcesParam resparam, String mODULE_PATH, final int transparency) {
+	public static void execHook_InvisiNotify(final InitPackageResourcesParam resparam, String MODULE_PATH, final int transparency) {
 		resparam.res.hookLayout("com.android.systemui", "layout", "super_status_bar", new XC_LayoutInflated() {
 			@Override
 			public void handleLayoutInflated(LayoutInflatedParam liparam) throws Throwable {
